8dfd8be355544698af31eb772f04c12ac89dac13
Aaron Neslin <aneslin125@gmail.com>
2022-01-25 15:53:28 -0500

    added readmen logic

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# dfadf
+[![license badge](https://img.shields.io/badge/license-MIT-green.svg)](https://shields.io/)
+
+## Installation
+asdfasdf  
+
+## Usage  
+asdfasdf
+
+## License
+This software is covered under the MIT license.  
+
+## Contribution
+adfasd  
+
+## Tests
+asdfasdf
+
+## Questions
+Please contact me at dadfadf with any question
+
+* (Github Profile)[https://github.com/asdfasd]
+
+
---
+++ app.js
@@ -0,0 +1,105 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+const questions = [
+    {name:"title",
+     type: "input",
+     message: "What is the title of your project?"    
+    },
+
+    {name:"description",
+    type: "input",
+    message: "Describe your project."    
+   },
+   {name:"installation",
+   type: "input",
+   message: "What is required to install your project?"    
+  },
+  {name:"usage",
+  type: "input",
+  message: "How is your project used?"    
+ },
+ {name:"license",
+ //make choices with license choices. use object
+ type: "list",
+ choices: ['MIT',
+  'GNU','BSD'],
+ message: "What is the license for your project?"    
+},
+{name:"contribution",
+type: "input",
+message: "How can people contribute to your project?"    
+},
+
+{name:"test",
+type: "input",
+message: "What tests are used in your project?"    
+},
+
+{
+name:"githubId",
+type: 'input',
+message: "What is your github user name"
+},
+{
+name: "email",
+type: "input",
+message: "What is your email address"
+}
+
+
+]
+
+inquirer.prompt(questions).then(response => {
+    
+
+
+    console.log(response)
+    const md = `
+# ${response.title}
+[![license badge](https://img.shields.io/badge/license-${response.license}-green.svg)](https://shields.io/)
+
+## Table of Contents
+
+![Installation](#Installation)
+![Description](#Description)
+![Usage](#Usage)
+![License](#License)
+![Contribution](#Contribution)
+![Tests](#Tests)
+![Questions](#Questions)  
+
+## Description
+${response.description}
+
+## Installation
+${response.installation}  
+
+## Usage  
+${response.usage}
+
+## License
+This software is covered under the ${response.license} license.  
+
+## Contribution
+${response.contribution}  
+
+## Tests
+${response.test}
+
+## Questions
+Please contact me at ${response.email} with any question
+
+* [Github Profile](https://github.com/${response.githubId})
+
+
+`
+
+
+    fs.writeFile('README.md', md, (err)=> {if (err) {console.log(err)}
+    else {
+        console.log("readme generated")
+    }
+    })
+})
